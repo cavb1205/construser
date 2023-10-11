13ee6bec7a6979f1e7da579cb2a6c67cc272fa4b
cavb1205 <cavb.1205@hotmail.com>
2023-10-11 18:56:07 -0300

    inicio astro contruser

---
+++ src/data/data.js
@@ -0,0 +1,54 @@
+export const preguntas = [
+  {
+    id: 1,
+    pregunta: "¿Como conseguir asesoria técnica?",
+    respuesta:
+      "Usted puede solicitar la Asesoría Técnica para nuestros productos y servicios, llamando directamente a los teléfonos: +56 9 8810 0833 y nuestros Técnicos lo asistirán para dar solución a su solicitud.",
+  },
+  {
+    id: 2,
+    pregunta: "Donde consigo los repuestos de los productos?",
+    respuesta:
+      "Usted puede solicitar los repuestos para nuestros productos y servicios, directamente en nuestra página web o llamando a los teléfonos: +56 9 8810 0833 y nuestros Técnicos lo asistirán para dar solución a su solicitud.",
+  },
+  {
+    id: 3,
+    pregunta: "¿Como puedo hacer un pedido?",
+    respuesta:
+      "Usted puede hacer un pedido directamente en nuestra página web o llamando a los teléfonos:+56 9 8810 0833 y nuestros Técnicos lo asistirán para dar solución a su solicitud.",
+  },
+  {
+    id: 4,
+    pregunta: "Direccion de la empresa y horarios de atención",
+    respuesta:
+      "Nuestra empresa se encuentra ubicada en la ciudad de Calama y Viña del Mar, y nuestros horarios de atención son: Lunes a Viernes de 8:00 am a 5:00 pm.",
+  }
+];
+
+
+export const servicios = [
+  {
+    id: 1,
+    nombre: "Servicio de Mantención",
+    descripcion: "Mantención de motores para portones automáticos",
+    
+  },
+  {
+    id: 2,
+    nombre: "Servicio de Instalación",
+    descripcion: "Instalación demotores para portones automáticos",
+    
+  },
+  {
+    id: 3,
+    nombre: "Servicio de Reparación",
+    descripcion: "Reparación de motores para portones automáticos",
+    
+  },
+  {
+    id: 4,
+    nombre: "Servicio de Venta",
+    descripcion: "Venta de motores para portones automáticos y repuestos",
+    
+  },
+];
